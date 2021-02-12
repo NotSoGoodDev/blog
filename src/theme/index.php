41ee012e3bca69969b39570743408ad425d903da
Not so good dev <iam@notsogood.dev>
2021-02-12 23:44:08 +0100

    Initial WordPress theme file

---
+++ src/theme/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Not so good website made by not so good dev</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<h1>I am <strong>not so good developer</strong> and yu are on not so good website</h1>
+	<?php wp_footer(); ?>
+</body>
+</html>
